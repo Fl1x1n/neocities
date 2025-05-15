27e1cd0138f5c5e7b54f09ef2f185a53aed192c8
Fl1x1n <fluxogamer1768@gmail.com>
2025-05-15 16:07:59 +0100

    F.R.C. - First Real Commit
    Finally started working on the personal website. So far so good. Right now I have the navbar (not working in functionality terms) but its already styled the way I want.

---
+++ src/RTC - Lisbon/scriptRTC.js
@@ -0,0 +1,14 @@
+function updateClock() {
+  const clockElement = document.getElementById('clock');
+  const lisbonTime = new Date().toLocaleTimeString('en-GB', {
+    timeZone: 'Europe/Lisbon',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+  clockElement.textContent = lisbonTime;
+}
+
+updateClock(); // Initial call
+setInterval(updateClock, 60* 1000); 
+
+
